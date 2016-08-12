37b1925b2ee99f42d91983da09e9b395d84cfe40
Ratnopam Chakrabarti <ratnopam.chakrabarti@ericsson.com>
2016-08-11 19:38:56 -0500

    Using DB Appender with PostGres DB

--- mydbappender/src/main/java/com/chakrar/logback/mydbappender/App.java
+++ mydbappender/src/main/java/com/chakrar/logback/mydbappender/App.java
@@ -1,13 +1,38 @@
 package com.chakrar.logback.mydbappender;
 
+import java.text.DateFormat;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
 /**
- * Hello world!
+ * Logback DB Appender
  *
  */
-public class App 
-{
+public class App {
+	
+	final static Logger log = LoggerFactory.getLogger(App.class);
+
+	
+	public App () {
+		log.info("Class instance created at {}", 
+                DateFormat.getInstance().format(new Date()));
+	}
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	log.warn("Running code...");
+        new App().appendToDb();
+        log.debug("Code execution complete.");
     }
+
+
+	private void appendToDb() {
+	      log.debug("In appendToDb");
+	      log.debug("appendToDb complete");
+		
+	}
 }
